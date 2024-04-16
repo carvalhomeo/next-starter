85180488495a0f8c56ae66935c7aa33bb58ec601
Marcel Carvalho <marcelcarvalho@MacBook-Pro.local>
2024-04-16 12:37:35 +0100

    Upgraded packages and added import formater

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import './globals.css'
+
+import { ReactNode } from 'react'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { ReactNode } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
